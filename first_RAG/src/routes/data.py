edadfcf7ca03a012052d6e8dbd49c3ad1cc71262
MaryamMaksour <maryammaksour@gmail.com>
2025-05-01 18:36:38 +0400

    process_endpoint

--- first_RAG/src/routes/data.py
+++ first_RAG/src/routes/data.py
@@ -4,10 +4,10 @@ import os
 import aiofiles
 from helpers.config import get_settings, Settings
 #from controllers.DataController import DataController   
-from controllers import DataController, ProjectController # using __init__.py in controllers
+from controllers import DataController, ProjectController, ProcessController # using __init__.py in controllers
 from models import ResponseSignal
 import logging
-
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -64,3 +64,28 @@ async def upload_data(project_id: str, file: UploadFile,
                "file_id": file_id
             }
         )
+
+
+
+@data_router.post("/process/{project_id}") 
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    chunk_overlap = process_request.overlap_size
+
+ 
+    process_controller = ProcessController(project_id=project_id)
+   
+    file_content = process_controller.get_file_content(file_id=file_id)
+    file_chunks = process_controller.process_file_content(file_content=file_content, file_id=file_id,
+                                                          chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {
+               "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+    
+    return file_chunks
